d61e76fe42a317e7d7e0b078431ba7a3822896a8
Natapong Sripeung <natapong.sripeung@gmail.com>
2021-05-22 15:09:44 +0700

    Change config

--- services.js
+++ services.js
@@ -1,4 +1,3 @@
-const functions = require('firebase-functions');
 const express = require('express');
 const request = require('request');
 const cors = require('cors');
@@ -8,7 +7,6 @@ const path = require('path');
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
 
-var port = process.env.PORT || 3000;
 app.get('/day/:days', (req, res) => {
     console.log("day get:",req.params.days)
     
@@ -27,4 +25,4 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'))
 })
 
-exports.sgn_test = functions.https.onRequest(app);
+app.listen(3000)
